d73d68ee40ac920ba560da8e324a2e3521631904
epic.chen <epicer.chen@gmail.com>
2022-04-09 14:29:09 +0800

    update plus one

--- plus-one.js
+++ plus-one.js
@@ -3,20 +3,16 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-    digits[digits.length - 1] = digits[digits.length - 1] + 1
+    digits[digits.length - 1]++;
 
-    if (digits[digits.length - 1] < 10) { return digits }
+    if (digits[digits.length - 1] < 10) { return digits; }
     for (let i = digits.length - 1; i >= 0; i--) {
-        if (parseInt(digits[i]) < 10) return digits
-        if (i == 0) digits[i] = 10
-        else {
-            digits[i] = 0
-            digits[i - 1] = digits[i - 1] + 1
+        if (digits[i] < 10) return digits;
+        digits[i] = 0;
+        if (i == 0) {
+            digits.splice(0, 0, 1)
+            return digits;
         }
+        digits[i - 1]++;
     }
-    if (digits[0] == 10) {
-        digits[0] = 0
-        digits.unshift(1)
-    }
-    return digits
 };
